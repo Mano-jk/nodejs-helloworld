295821ea5868a02931423c875d9bed0ba8cfc358
Manoj <32917899+Mano-jk@users.noreply.github.com>
2022-02-23 22:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
+  var timeleft = 10;
+  var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
